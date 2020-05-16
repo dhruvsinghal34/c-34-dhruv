b522cbdbc1954f88388fa69b756d622266a75f35
dhruvsinghal34 <drmonikagupta16@gmail.com>
2020-05-16 16:06:38 +0530

    internet

--- sketch.js
+++ sketch.js
@@ -1,29 +1,39 @@
 var ball;
-
+var database;
 function setup(){
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    database=firebase.database();
+    var childNodRef=database.ref('ball/position');
+    childNodRef.on("value",function(data){
+        var position=data.val();
+        ball.x=position.x;
+        ball.y=position.y;
+    });
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x1,y1){
+var childNodRef1=database.ref('ball/position') ;
+childNodRef1.set({
+    x:ball.x+x1,
+    y:ball.y+y1
+});
 }
